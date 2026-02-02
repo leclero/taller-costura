2c138a21dc33d4c359fb8e1daf3e7d9330b35f27
Leclero <leli322003@gmail.com>
2026-02-02 18:12:47 -0300

    Actualiza la URL de la API en AdminView.vue para el envío de datos de prendas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// Importar Rutas
 const prendaRoutes = require('./routes/prendaRoutes');
 
 // 1. Middlewares (ORDEN CRÍTICO)
@@ -21,6 +23,7 @@ app.use('/api/prendas', prendaRoutes);
 
 app.get('/', (req, res) => res.send("API Funcionando 🪡"));
 
+// --- PUERTO Y ARRANQUE ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Puerto: ${PORT}`);
