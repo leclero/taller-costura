1bedf383fea4112b53a83594b0141d381514199e
Leclero <leli322003@gmail.com>
2026-02-02 21:09:45 -0300

    Limpieza de estructura: eliminado archivos basura de Vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,16 +5,8 @@ import AdminView from '../views/AdminView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView
-    }
+    { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
+    { path: '/admin', name: 'admin', component: () => import('../views/AdminView.vue') }
   ]
 })
 
