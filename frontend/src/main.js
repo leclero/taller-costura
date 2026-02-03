93e7f683e9e9bc5c2bb06ecee190f9473ac88346
Leclero <leli322003@gmail.com>
2026-02-02 21:39:36 -0300

    Limpieza total de archivos duplicados en la raíz

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import App from '@/App.vue'
 import router from './router'
-import './assets/main.css' // Solo este
+import '@/assets/main.css' // Solo este
 
 const app = createApp(App)
 app.use(createPinia())
