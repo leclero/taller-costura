dbe7ed8117df70de509531d1a60139a28943690f
Leclero <leli322003@gmail.com>
2026-02-04 21:19:36 -0300

    Fix: Importación de mongoose en modelo Usuario

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose'); // <--- ESTA ES LA LÍNEA QUE FALTA
+
 const usuarioSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   rol: { 
     type: String, 
-    // Añadimos los nuevos roles aquí
     enum: ['dueño', 'admin', 'Vendedor', 'Ayudante'], 
     default: 'admin' 
   }
 });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
